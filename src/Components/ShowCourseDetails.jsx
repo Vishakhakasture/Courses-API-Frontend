b8bc59e79c420e9e018d770df19e3694e8d1fae0
vishakha <vishakhakasture302002@gmail.com>
2024-09-04 23:06:18 +0530

    initial commit

--- src/Components/ShowCourseDetails.jsx
+++ src/Components/ShowCourseDetails.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { FaTrash, FaSearch } from "react-icons/fa";
 import axios from "../services/api";
 
